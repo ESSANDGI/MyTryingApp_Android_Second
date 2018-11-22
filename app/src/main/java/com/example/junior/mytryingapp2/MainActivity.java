d54faceb3af99b49a7f57362f19ec9108efeb085
ESSANDGI <juniornelson.sulcagonzales@gmail.com>
2018-11-22 16:40:35 +0100

    Aggiunti metodi che gestiscono i cicli di vita di un app

--- app/src/main/java/com/example/junior/mytryingapp2/MainActivity.java
+++ app/src/main/java/com/example/junior/mytryingapp2/MainActivity.java
@@ -11,6 +11,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "app creata", Toast.LENGTH_LONG).show();
-        
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+//      l'attività stà per rendersi visibile
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+//      l'attività è visibile
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+//      concentrarsi su altra attività
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+//      l'attività non è più visibile
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+//      l'attività è a punto di essere distrutta
     }
 }
