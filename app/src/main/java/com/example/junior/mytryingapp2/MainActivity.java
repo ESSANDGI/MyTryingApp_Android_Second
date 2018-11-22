c11137050b2cde1a3209a371493f15141f368926
ESSANDGI <juniornelson.sulcagonzales@gmail.com>
2018-11-22 20:08:46 +0100

    aggiunta parte logica all'applicazione ed assegnato evento al bottone

--- app/src/main/java/com/example/junior/mytryingapp2/MainActivity.java
+++ app/src/main/java/com/example/junior/mytryingapp2/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.junior.mytryingapp2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+    private EditText et3;
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "app creata", Toast.LENGTH_LONG).show();
 
+        et1 = (EditText)findViewById(R.id.txt_1);
+        et2 = (EditText)findViewById(R.id.txt_2);
+        et3 = (EditText)findViewById(R.id.txt_3);
+        tv1 = (TextView)findViewById(R.id.view_1);
+    }
+
+    public void calculate(View view){
+        double val1 = Double.parseDouble(et1.getText().toString());
+        double val2 = Double.parseDouble(et2.getText().toString());
+        double val3 = Double.parseDouble(et3.getText().toString());
+        double result = (val1 + val2 + val3)/3;
+        if(result >= 6.0)
+            tv1.setText(String.format("Status: Promosso con %.3f", result));
+        else
+            tv1.setText(String.format("Status: Bocciato con %.3f", result));
     }
 
     @Override
